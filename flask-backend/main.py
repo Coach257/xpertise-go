dd17c4638b6036bcab555b119c24a3c2539a0781
Yinghao Zhu <zhuyinghao1999@msn.cn>
2020-11-24 21:30:07 +0800

    try flask flexible env
    Former-commit-id: 52f41e1a11bb5dca225b4c8338b37280fc80de05

--- flask-backend/main.py
+++ flask-backend/main.py
@@ -10,6 +10,6 @@ def search_author_by_name():
     res=next(scholarly.search_author(author_name)).affiliation
     return jsonify(res)
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", debug=True)
+if __name__ == '__main__':
+    app.run(host="127.0.0.1", port=8080, debug=True)
 
