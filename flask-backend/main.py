3f99a83296e0656763b4ef5c681988e6c28849c0
Yinghao Zhu <zhuyinghao1999@msn.cn>
2020-11-24 20:39:28 +0800

    to test: flask version
    Former-commit-id: b3f9c34cce4f3533f3c821f2c232e894862e4587

---
+++ flask-backend/main.py
@@ -0,0 +1,15 @@
+from flask import Flask, request, jsonify
+
+
+app=Flask(__name__)
+
+@app.route('/api/search/',methods=['GET','POST'])
+def search_author_by_name():
+    from scholarly import scholarly
+    author_name='Mark'
+    res=next(scholarly.search_author(author_name)).affiliation
+    return jsonify(res)
+
+if __name__=='__main__':
+    app.run(port=8080)
+
