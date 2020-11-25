a2b5479957b20a3a7f3a897e709b07a568b463b4
Coach257 <45021066+Coach257@users.noreply.github.com>
2020-11-25 18:01:20 +0800

    Revert "app engine connect sql"
    This reverts commit 176306d1f65285d6679846bbe48050494e6778ff [formerly 8b73d00420a3c8d692eef420b1b0f01e80e49342].
    
    
    Former-commit-id: d79afd22d792b3436729198531278a87c774d1ef

--- gin-project/dao/config.go
+++ gin-project/dao/config.go
@@ -1,9 +1,6 @@
 package dao
 
 import (
-	"fmt"
-	"os"
-
 	"github.com/jinzhu/gorm"
 )
 
@@ -12,15 +9,7 @@ var (
 )
 
 func InitMySQL() (err error) {
-	socketDir, isSet := os.LookupEnv("DB_SOCKET_DIR")
-	if !isSet {
-		socketDir = "/cloudsql"
-	}
-
-	var dbURI string
-	dbURI = fmt.Sprintf("szl:123@unix(/%s/test-project-296508:asia-east2:test)/test?parseTime=true", socketDir)
-
-	DB, err = gorm.Open("mysql", dbURI)
+	DB, err = gorm.Open("mysql", "root:@buaa21@tcp(101.132.227.56:3306)/test?charset=utf8&parseTime=True&loc=Local")
 	if err != nil {
 		panic(err)
 	}
--- gin-project/dao/model.go
+++ gin-project/dao/model.go
@@ -1,6 +1,6 @@
 package dao
 
 type User struct {
-	ID   int64  `gorm:"primary_key"`
+	ID   uint   `gorm:"primary_key"`
 	Name string `gorm:"size:255"` // string默认长度为255, 使用这种tag重设。
 }
