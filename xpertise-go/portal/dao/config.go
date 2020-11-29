22b60cb7ac668071c2814d8368c1cb89a7d1194a
BingOAO233 <wyhchris@qq.com>
2020-11-29 23:52:02 +0800

    Update portal/dao/config.go & model.go  (db model version 0.1)
    Former-commit-id: c488563fc379149a63a84cfbab18217e7ea7cdfd

--- xpertise-go/portal/dao/config.go
+++ xpertise-go/portal/dao/config.go
@@ -11,10 +11,15 @@ var (
 
 // InitMySQL means initialize database.
 func InitMySQL() (err error) {
-	DB, err = gorm.Open("mysql", "root:@buaa21@tcp(101.132.227.56:3306)/portal_db?charset=utf8&parseTime=True&loc=Local")
+	DB, err = gorm.Open("mysql", "root:@buaa21@tcp(101.132.227.56:3306)/admin_db?charset=utf8&parseTime=True&loc=Local")
 	if err != nil {
 		panic(err)
 	}
+
+	DB.Set("gorm:table_options", "ENGINE=InnoDB").CreateTable(&Organization{})
+	DB.Set("gorm:table_options", "ENGINE=InnoDB").CreateTable(&Document{})
+	DB.Set("gorm:table_options", "ENGINE=InnoDB").CreateTable(&OrgDocument{})
+	DB.Set("gorm:table_options", "ENGINE=InnoDB").CreateTable(&OrgPatent{})
 
 	// 为模型`User`创建表
 	// DB.CreateTable(&User{})
@@ -30,5 +35,5 @@ func InitMySQL() (err error) {
 
 // Close the connection of the database.
 func Close() {
-	DB.Close()
+	_ = DB.Close()
 }
--- xpertise-go/portal/dao/model.go
+++ xpertise-go/portal/dao/model.go
@@ -1,5 +1,39 @@
 package dao
 
+import (
+	"database/sql/driver"
+	"github.com/json-iterator/go"
+	"time"
+)
+
+// customized type for string array store & read
+type StrList []string
+
+func (l StrList) Value() (driver.Value, error) {
+	data, err := jsoniter.Marshal(l)
+	return string(data), err
+}
+
+func (l *StrList) Scan(data interface{}) error {
+	return jsoniter.Unmarshal(data.([]byte), l)
+}
+
+// customized type for uint array store & read
+type UintList []uint
+
+func (l UintList) Value() (driver.Value, error) {
+	data, err := jsoniter.Marshal(l)
+	return string(data), err
+}
+
+func (l *UintList) Scan(data interface{}) error {
+	err := jsoniter.Unmarshal(data.([]byte), l)
+	for i := 0; i < len(*l); i++ {
+		print((*l)[i])
+	}
+	return err
+}
+
 // User Model.
 type User struct {
 	ID   uint64 `gorm:"primary_key"`
@@ -11,4 +45,47 @@ type Student struct {
 	ID   uint64 `gorm:"primary_key"`
 	Name string `gorm:"size:255"`
 	Age  uint64
+}
+
+// Organization Model.
+type Organization struct {
+	OrgID        uint64 `gorm:"primary_key;"`
+	OrgNameCn    string `gorm:"size:20;not null;unique"`
+	OrgNameEn    string `gorm:"size:40;not null;unique"`
+	OrgNameEns   string `gorm:"size:10;"`
+	BasicInfo    string `gorm:"size:200;"`
+	SumOfDocs    uint32
+	SumOfPatents uint32
+	AvgScore     float32
+	AdminID      uint64   `gorm:"not null;"`
+	PortalList   UintList `gorm:"type:varchar(200)" json:"portal_list"`
+}
+
+// Document Model.
+type Document struct {
+	DocID       uint64  `gorm:"primary_key;"`
+	AuthorList  StrList `gorm:"type:varchar(200)" json:"author_list"`
+	TypeList    StrList `gorm:"type:varchar(200)" json:"type_list"`
+	Abstract    string  `gorm:"size:150"`
+	CiteList    StrList `gorm:"type:varchar(500)" json:"cite_list"`
+	PublishTime time.Time
+	Source      string `gorm:"size:30"`
+	Original    string `gorm:"size:100"`
+}
+
+// OrgDocument Model.
+type OrgDocument struct {
+	OrgID uint64 `gorm:"ForeignKey:OrgID"`
+	DocID uint64 `gorm:"ForeignKey:DocID"`
+}
+
+// OrgPatent Model.
+type OrgPatent struct {
+	PatentID     uint64  `gorm:"primary_key;"`
+	OrgID        uint64  `gorm:"ForeignKey:OrgID;not null;"`
+	InventorList StrList `gorm:"type:varchar(200);not null;" json:"inventor_list"`
+	PatentType   int
+	PatentCode   string `gorm:"size:20"`
+	PatentClass  string `gorm:"size:40"`
+	AgentName    string `gorm:"size:20"`
 }
