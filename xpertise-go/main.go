b02d8a16228d2100dc6c5a2c289ed83152202be5
Yinghao Zhu <zhuyinghao1999@msn.cn>
2020-11-30 18:13:46 +0800

    fix router error (refactor router structure)
    Former-commit-id: a121cb3dd6083b3ed7c12b34b46f15b097a4f003

--- xpertise-go/main.go
+++ xpertise-go/main.go
@@ -2,13 +2,10 @@ package main
 
 import (
 	adminDao "xpertise-go/admin/dao"
-	adminRouter "xpertise-go/admin/router"
 	branchDao "xpertise-go/branch/dao"
-	branchRouter "xpertise-go/branch/router"
 	portalDao "xpertise-go/portal/dao"
-	portalRouter "xpertise-go/portal/router"
+	"xpertise-go/router"
 	userDao "xpertise-go/user/dao"
-	userRouter "xpertise-go/user/router"
 
 	_ "github.com/jinzhu/gorm/dialects/mysql"
 )
@@ -19,30 +16,25 @@ func main() {
 		panic(userErr)
 	}
 	defer userDao.Close()
-	userR := userRouter.SetupRouter()
-	userR.Run()
 
 	adminErr := adminDao.InitMySQL()
 	if adminErr != nil {
 		panic(adminErr)
 	}
 	defer adminDao.Close()
-	adminR := adminRouter.SetupRouter()
-	adminR.Run()
 
 	branchErr := branchDao.InitMySQL()
 	if branchErr != nil {
 		panic(branchErr)
 	}
 	defer branchDao.Close()
-	branchR := branchRouter.SetupRouter()
-	branchR.Run()
 
 	portalErr := portalDao.InitMySQL()
 	if portalErr != nil {
 		panic(portalErr)
 	}
 	defer portalDao.Close()
-	portalR := portalRouter.SetupRouter()
-	portalR.Run()
+
+	r := router.SetupRouter()
+	r.Run()
 }
