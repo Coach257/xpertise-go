9c5bde18c4d50e6b7055d5066c902d7afaef5f49
Yinghao Zhu <zhuyinghao1999@msn.cn>
2020-12-01 00:17:41 +0800

    clear more
    Former-commit-id: f4059811be8d9c0fa4dd9cb0e956190a52393544

--- xpertise-go/router/router.go
+++ xpertise-go/router/router.go
@@ -1,8 +1,6 @@
 package router
 
 import (
-	adminController "xpertise-go/admin/controller"
-	branchController "xpertise-go/branch/controller"
 	portalController "xpertise-go/portal/controller"
 	userController "xpertise-go/user/controller"
 
@@ -13,37 +11,22 @@ import (
 func SetupRouter() *gin.Engine {
 	r := gin.Default()
 
-	adminV1 := r.Group("api/v1/admin")
-	{
-		adminV1.POST("/create", adminController.CreateAStudent)
-		adminV1.DELETE("/delete/:id", adminController.DeleteAStudentByID)
-		adminV1.PUT("/update/age", adminController.UpdateAStudentByAge)
-		adminV1.GET("/query/all", adminController.QueryAllStudents)
-		adminV1.GET("/query/id", adminController.QueryStudentByID)
-		adminV1.GET("/query/age", adminController.QueryStudentsByAge)
-	}
-	branchV1 := r.Group("api/v1/branch")
-	{
-		branchV1.POST("/create", branchController.CreateAStudent)
-		branchV1.DELETE("/delete/:id", branchController.DeleteAStudentByID)
-		branchV1.PUT("/update/age", branchController.UpdateAStudentByAge)
-		branchV1.GET("/query/all", branchController.QueryAllStudents)
-		branchV1.GET("/query/id", branchController.QueryStudentByID)
-		branchV1.GET("/query/age", branchController.QueryStudentsByAge)
-	}
+	// adminV1 := r.Group("api/v1/admin")
+
+	// branchV1 := r.Group("api/v1/branch")
+
 	portalV1 := r.Group("api/v1/portal")
 	{
-		portalV1.POST("/document/create", portalController.CreateADocument)
 		portalV1.GET("/document/query", portalController.QueryDocumentByID)
 	}
 	userV1 := r.Group("api/v1/user")
 	{
 		userV1.POST("/create", userController.CreateAUser)
-		userV1.DELETE("/delete/:id", userController.DeleteAStudentByID)
-		userV1.PUT("/update/age", userController.UpdateAStudentByAge)
-		userV1.GET("/query/all", userController.QueryAllStudents)
-		userV1.GET("/query/id", userController.QueryStudentByID)
-		userV1.GET("/query/age", userController.QueryStudentsByAge)
+		// userV1.DELETE("/delete/:id", userController.DeleteAStudentByID)
+		// userV1.PUT("/update/age", userController.UpdateAStudentByAge)
+		// userV1.GET("/query/all", userController.QueryAllStudents)
+		// userV1.GET("/query/id", userController.QueryStudentByID)
+		// userV1.GET("/query/age", userController.QueryStudentsByAge)
 	}
 
 	return r
