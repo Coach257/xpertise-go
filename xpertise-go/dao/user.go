a0355ebdd8a75611bfbbeb7b7023c94261bd3bf4
Rolin <1207640183@qq.com>
2020-12-01 12:21:22 +0800

    User table: add unique constraint to username email;set interdiction default value to false
    Former-commit-id: f1020f1d115b72eadec2e4731bda388803403792

--- xpertise-go/dao/user.go
+++ xpertise-go/dao/user.go
@@ -2,12 +2,12 @@ package dao
 
 type User struct {
 	UserID       uint64 `gorm:"primary_key; not null"`
-	Username     string `gorm:"size:15; not null"`
+	Username     string `gorm:"size:15; not null; unique"`
 	Password     string `gorm:"size:20; not null"`
-	Email        string `gorm:"size:20; not null"`
+	Email        string `gorm:"size:20; not null; unique"`
 	Usertype     int    `gorm:"not null;default:1"`
 	BasicInfo    string `gorm:"size:100"`
-	Interdiction bool
+	Interdiction bool   `gorm:"default:false"`
 
 	//User_type Profile `gorm:"ForeignKey:UserRefer"` 指定外键
 }
