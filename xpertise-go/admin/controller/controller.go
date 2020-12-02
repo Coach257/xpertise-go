26a6cb1f4c7939c327596c854b0e65fdb7cd225e
74LS73 <664185376@qq.com>
2020-12-02 20:25:33 +0800

    【admin】fix a little bug in dao and add in server
    Former-commit-id: 0a193962ca82cb59c2daf3f8406177d3de358fe7

--- xpertise-go/admin/controller/controller.go
+++ xpertise-go/admin/controller/controller.go
@@ -1,1 +1,13 @@
 package controller
+
+// DealWithAComReport ll
+// func DealWithAComReport(c *gin.Context) bool {
+// 	commentID := c.Request.FormValue("comment_id")
+// 	deal := c.Request.FormValue("deal")
+
+// 	if _, notfound := server.QueryACommentByID(comment_id); notfound != true {
+
+// 		// return true;
+// 	}
+// 	return
+// }
