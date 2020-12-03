9965d3a81edf0e69de482ac2019a183b3fbd8c20
Rolin <1207640183@qq.com>
2020-12-03 12:55:49 +0800

    update API Doc.md
    Former-commit-id: f20131612ef01fcb19618533f0d0e8b556288453

--- xpertise-go/user/auth/auth.go
+++ xpertise-go/user/auth/auth.go
@@ -21,7 +21,7 @@ var (
 )
 
 type CustomClaims struct {
-	UserID 	 uint64 `json:"userid"`
+	UserID 	 uint64 `json:"user_id"`
 	Username string `json:"username"`
 	Email	 string `json:"email"`
 	jwt.StandardClaims
