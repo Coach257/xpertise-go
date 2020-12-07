81a4294d41a2bdc0b1146bfcd207e12a73edc42e
Rolin <1207640183@qq.com>
2020-12-07 16:21:16 +0800

    auth:  "token" ->"authorization"
    Former-commit-id: 7fc04eb09cf4f574ec33d5e187b0bb765ecdf88e

--- user/auth/auth.go
+++ user/auth/auth.go
@@ -75,7 +75,6 @@ func JwtAuth() gin.HandlerFunc {
 	return func(c *gin.Context) {
 		// 尝试获取请求头部的token
 		token := c.Request.Header.Get("authorization")
-		//token:=c.Request.FormValue("token")
 
 		// 如果token为空，那就直接return
 		if token == "" {
