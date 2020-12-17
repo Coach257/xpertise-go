d60420a5c52763cc6944ad58180a85ec8305b9f3
ztx <1213587739@qq.com>
2020-12-17 21:03:13 +0800

    按authorid返回colid
    Former-commit-id: 51a445c4e86a539c5d7d8cd8b8accd361a02a9c8

--- api/v1/portal.go
+++ api/v1/portal.go
@@ -25,7 +25,23 @@ func CreateSpecialColumn(c *gin.Context) {
 	c.JSON(http.StatusOK, gin.H{"success": true, "message": "创建专栏成功"})
 	return
 }
-
+// SearchSpecialColumn doc
+// @description 返回某个作者的一个专栏
+// @Tags portal
+// @Param author_id formData string true "作者ID"
+/
+// @Success 200 {string} string "{"success": true, "message": "创建专栏成功"}"
+// @Router /portal/create_column [post]
+func CreateSpecialColumn(c *gin.Context) {
+	authorID := c.Request.FormValue("author_id")
+	
+	if data,err := service.QueryAColumnByID(authorID); err != nil {
+		c.JSON(http.StatusOK, gin.H{"success": false, "message": err.Error()})
+		return
+	}
+	c.JSON(http.StatusOK, gin.H{"success": true, "message": "返回专栏成功","data":data})
+	return
+}
 // AddToColumn doc
 // @description 添加某篇文章到专栏
 // @Tags portal
